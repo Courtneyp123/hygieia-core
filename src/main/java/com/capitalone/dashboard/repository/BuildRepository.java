dd79a452f24d959ae130a1d2d5a7054f7b61c888
Courtney Peng <courtney.peng@gmail.com>
2021-01-28 19:26:40 -0500

    added to build repository search for top 10 by collector item id

--- src/main/java/com/capitalone/dashboard/repository/BuildRepository.java
+++ src/main/java/com/capitalone/dashboard/repository/BuildRepository.java
@@ -28,6 +28,8 @@ public interface BuildRepository extends CrudRepository<Build, ObjectId>, QueryD
 
     Build findTop1ByCollectorItemIdOrderByTimestampDesc(ObjectId collectorItemId);
 
+    List<Build> findTop10ByCollectorItemIdOrderByTimestampDesc(ObjectId collectorItemId);
+
     Build findTop1ByCollectorItemIdAndBuildStatusOrderByTimestampDesc(ObjectId collectorItemId, BuildStatus buildStatus);
 
     Build findTop1ByCollectorItemIdAndTimestampIsBetweenOrderByTimestampDesc(ObjectId collectorItemId, long beginDate, long endDate);
